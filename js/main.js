4de2bad5ee43d63681ee2f05aa55d3d428273e39
Larry González <larry.g.m1024@gmail.com>
2022-10-18 22:26:49 -0300

    actuallity page

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@ window.addEventListener('load', ()=>{
     grid.refreshItems().layout();
     document.getElementById('grid').classList.add('imagenes-cargadas');
 
+    //listeners para filtrar por categoria
     const enlaces= document.querySelectorAll('#categorias a');
     enlaces.forEach((elemento)=>{
         elemento.addEventListener('click',(evento)=>{
@@ -18,5 +19,36 @@ window.addEventListener('load', ()=>{
             const categoria = evento.target.innerHTML.toLowerCase();
             categoria === 'todos' ? grid.filter('[data-categoria]'): grid.filter(`[data-categoria= "${categoria}"]`);
         })
+    })
+
+    //listener para el buscador
+    document.querySelector('#barra-busqueda').addEventListener('input', (evento)=>{
+        const busqued=evento.target.value;
+        console.log(busqued);
+        grid.filter((item) => item.getElement().dataset.etiquetas.includes(busqued));
+    })
+
+    //listener para las imagenes
+    const overlay = document.getElementById('overlay');
+    document.querySelectorAll('.grid .item img').forEach((elemento)=>{
+        
+        elemento.addEventListener('click', ()=>{
+            const ruta = elemento.getAttribute('src');
+            const descr = elemento.parentNode.parentNode.dataset.descripcion;
+            overlay.classList.add('activo');
+            document.querySelector('#overlay img').src=ruta;
+            document.querySelector('#overlay .descripcion').innerHTML=descr;
+        })
+    });
+
+    //    event listener del boton cerrar
+    document.querySelector('#btn-cerrar-popup').addEventListener('click', ()=>{
+        overlay.classList.remove('activo');
+
+    // event listener 
+        overlay.addEventListener('click', (evento) => {
+           evento.target.id ==='overlay'? overlay.classList.remove('activo') : '';
+        });
+
     })
 });
